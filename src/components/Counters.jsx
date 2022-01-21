0302fa9f939d63242bd0335d8a01aa05f126b46d
Kanishka Samarathunga <kanishkakcs@gmail.com>
2022-01-22 01:04:40 +0530

    new components added

---
+++ src/components/Counters.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Counter from './Counter';
+
+
+class Counters extends React.Component {
+    
+    
+    render() {
+        const { onReset, counters, onDelete, onIncrement, onDecrement } = this.props;
+        
+        return (
+            <div>
+                <button 
+                    onClick={onReset}
+                    className="btn btn-primary btn-sm m-2">Reset
+                </button>
+                {counters.map(counter => (
+                    <Counter 
+                        key={counter.id} 
+                        onDelete={onDelete}
+                        onIncrement={onIncrement}
+                        counter={counter}
+                        onDecrement={onDecrement}
+                    /> 
+                ))}
+            </div>
+        );
+    }
+}
+ 
+export default Counters;
